fe4a44af626301aa3a473ae80d55a7dd8e2acbd8
Morten V. Pedersen <morten@mortenvp.com>
2022-12-22 14:02:53 +0100

    Fix run_async
    * Fix run_async

--- test/test_dummynet.py
+++ test/test_dummynet.py
@@ -7,14 +7,17 @@ import dummynet
 import logging
 import time
 import pytest
+import os
 
 
 def test_run():
 
     log = logging.getLogger("dummynet")
 
+    sudo = False if os.geteuid() == 0 else True
+
     # The host shell used if we don't have a recording
-    shell = HostShell(log=log, sudo=True, process_monitor=None)
+    shell = HostShell(log=log, sudo=sudo, process_monitor=None)
 
     # Create a mock shell which will receive the calls performed by the DummyNet
     # shell = mockshell.MockShell()
@@ -84,6 +87,62 @@ def test_run():
         net.cleanup()
 
 
+def test_run_async():
+
+    log = logging.getLogger("dummynet")
+
+    sudo = False if os.geteuid() == 0 else True
+
+    process_monitor = ProcessMonitor()
+
+    shell = HostShell(log=log, sudo=sudo, process_monitor=process_monitor)
+
+    net = DummyNet(shell=shell)
+
+    try:
+
+        # Get a list of the current namespaces
+        namespaces = net.netns_list()
+        assert namespaces == []
+
+        # create two namespaces
+        demo0 = net.netns_add(name="demo0")
+        demo1 = net.netns_add(name="demo1")
+
+        net.link_veth_add(p1_name="demo0-eth0", p2_name="demo1-eth0")
+
+        # Move the interfaces to the namespaces
+        net.link_set(namespace="demo0", interface="demo0-eth0")
+        net.link_set(namespace="demo1", interface="demo1-eth0")
+
+        # Bind an IP-address to the two peers in the link.
+        demo0.addr_add(ip="10.0.0.1/24", interface="demo0-eth0")
+        demo1.addr_add(ip="10.0.0.2/24", interface="demo1-eth0")
+
+        # Activate the interfaces.
+        demo0.up(interface="demo0-eth0")
+        demo1.up(interface="demo1-eth0")
+        demo0.up(interface="lo")
+        demo1.up(interface="lo")
+
+        proc0 = demo0.run_async(cmd="ping -c 10 10.0.0.2")
+        proc1 = demo1.run_async(cmd="ping -c 10 10.0.0.1")
+
+        # process_monitor.add_process(proc0)
+        # process_monitor.add_process(proc1)
+
+        while process_monitor.run():
+            pass
+
+        proc0.result.match(stdout="10 packets transmitted*", stderr=None)
+        proc1.result.match(stdout="10 packets transmitted*", stderr=None)
+
+    finally:
+
+        # Clean up.
+        net.cleanup()
+
+
 def test_with_timeout():
 
     log = logging.getLogger("dummynet")
@@ -130,11 +189,13 @@ def test_daemon_exit():
     log.setLevel(logging.DEBUG)
     log.addHandler(logging.StreamHandler())
 
+    sudo = False if os.geteuid() == 0 else True
+
     # Create a process monitor
     process_monitor = ProcessMonitor()
 
     # The host shell used if we don't have a recording
-    shell = HostShell(log=log, sudo=True, process_monitor=process_monitor)
+    shell = HostShell(log=log, sudo=sudo, process_monitor=process_monitor)
 
     try:
 
@@ -159,11 +220,13 @@ def test_all_daemons():
     log.setLevel(logging.DEBUG)
     log.addHandler(logging.StreamHandler())
 
+    sudo = False if os.geteuid() == 0 else True
+
     # Create a process monitor
     process_monitor = ProcessMonitor()
 
     # The host shell used if we don't have a recording
-    shell = HostShell(log=log, sudo=True, process_monitor=process_monitor)
+    shell = HostShell(log=log, sudo=sudo, process_monitor=process_monitor)
 
     try:
 
@@ -212,8 +275,10 @@ def test_pendingresult():
     # Create a process monitor
     process_monitor = ProcessMonitor()
 
+    sudo = False if os.geteuid() == 0 else True
+
     # The host shell used if we don't have a recording
-    shell = HostShell(log=log, sudo=True, process_monitor=process_monitor)
+    shell = HostShell(log=log, sudo=sudo, process_monitor=process_monitor)
 
     # DummyNet wrapper that will prevent clean up from happening in playback
     # mode if an exception occurs
@@ -247,8 +312,10 @@ def test_process_still_running():
     # Create a process monitor
     process_monitor = ProcessMonitor()
 
+    sudo = False if os.geteuid() == 0 else True
+
     # The host shell used if we don't have a recording
-    shell = HostShell(log=log, sudo=True, process_monitor=process_monitor)
+    shell = HostShell(log=log, sudo=sudo, process_monitor=process_monitor)
 
     # DummyNet wrapper that will prevent clean up from happening in playback
     # mode if an exception occurs
@@ -267,3 +334,6 @@ def test_process_still_running():
 
         # Close any running async commands
         process_monitor.stop()
+
+        # Clean up.
+        net.cleanup()
